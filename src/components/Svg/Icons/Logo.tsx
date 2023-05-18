21161d0928b4417f5775306b8e747ab39c26becc
Matt <mmccann@ssolutions.ca>
2023-05-17 23:19:33 -0400

    make logo wider

--- src/components/Svg/Icons/Logo.tsx
+++ src/components/Svg/Icons/Logo.tsx
@@ -4,8 +4,8 @@ import { SvgProps } from "../types";
 
 const Icon: React.FC<SvgProps> = (props) => {
   return (
-    <Svg viewBox="0 0 32 32" {...props}>
-      <image width="32" height="32" href={`${process.env.PUBLIC_URL}/images/egg/LogoTextNewDark.png`} />
+    <Svg viewBox="0 0 64 32" {...props}>
+      <image width="64" height="32" href={`${process.env.PUBLIC_URL}/images/egg/LogoTextNewDark.png`} />
     </Svg>
   );
 };
